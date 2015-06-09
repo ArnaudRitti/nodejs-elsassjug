8d96769446745b56e9a9393b7a0a76e6f4f0c8f2
Arnaud RITTI <arnaud.ritti@gmail.com>
2015-06-09 19:52:34 +0200

    Etape 3 : Votre premier serveur HTTP

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express')
+var app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.get('/pets', function (req, res) {
+    var Heidi = new Object();
+    Heidi.name = "Heidi";
+    Heidi.kind  = 'Dog';
+    Heidi.age = 3;
+
+    var Plutot = new Object();
+    Plutot.name = "Plutot";
+    Plutot.kind  = 'Dog';
+    Plutot.age = 14;
+
+    var pets = [Heidi, Plutot];
+  res.send(JSON.stringify(pets))
+})
+
+app.listen(3000)
