3c7d4a6ce20d2888234584eec2be800310594a94
Arnaud RITTI <arnaud.ritti@gmail.com>
2015-06-09 20:00:38 +0200

    Etape 4 Grunt

--- server.js
+++ server.js
@@ -20,4 +20,19 @@ app.get('/pets', function (req, res) {
   res.send(JSON.stringify(pets))
 })
 
+app.post('/pets', function (req, res) {
+    var Heidi = new Object();
+    Heidi.name = "Heidi";
+    Heidi.kind  = 'Dog';
+    Heidi.age = 3;
+
+    var Plutot = new Object();
+    Plutot.name = "Plutot";
+    Plutot.kind  = 'Dog';
+    Plutot.age = 14;
+
+    var pets = [Heidi, Plutot];
+  res.send(JSON.stringify(pets))
+})
+
 app.listen(3000)
