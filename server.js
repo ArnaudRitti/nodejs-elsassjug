694fa26aef76c7357bdb351b6bf3ed347158b031
Arnaud RITTI <arnaud.ritti@gmail.com>
2015-06-09 21:11:39 +0200

    SWIG HTML

--- server.js
+++ server.js
@@ -2,18 +2,30 @@ var express = require('express')
 var bodyParser = require('body-parser')
 var Firebase = require("firebase");
 var myFirebaseRef = new Firebase("https://sizzling-torch-8158.firebaseio.com/");
+var swig  = require('swig');
+
+
 var app = express()
 
 app.use(bodyParser.json())
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/tpl');
 
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
 app.get('/pets', function (req, res) {
+    var datasJSON = null;
     myFirebaseRef.once("value", function(data) {
-        res.send(data.val())
+        var datasJSON = data.val();
+        res.render('list', {
+            pagename: 'Liste of Datas',
+            datas: datasJSON
+        });
     });
+
 })
 
 
